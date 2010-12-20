935d27d1487239eb46c7d4920191b07f4ded8f31
Naveen Kalla <nkalla@codeaurora.org>
2010-12-20 14:37:49 -0800

    Add group qcom_oncrpc and give permissions to ril-daemon and /dev/oncrpc
    Change-Id: I022aae4a81c720a026ea6c1a41bda30acbe60224
    
    Conflicts:
    	include/private/android_filesystem_config.h

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -91,6 +91,7 @@
 #define AID_NET_BW_STATS  3006  /* read bandwidth statistics */
 #define AID_NET_BW_ACCT   3007  /* change bandwidth statistics accounting */
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
+#define AID_QCOM_ONCRPC   3009  /* can read/write /dev/oncrpc files */
 
 #define AID_MISC          9998  /* access to misc storage */
 #define AID_NOBODY        9999
@@ -163,6 +164,7 @@ static const struct android_id_info android_ids[] = {
     { "net_admin",     AID_NET_ADMIN, },
     { "net_bw_stats",  AID_NET_BW_STATS, },
     { "net_bw_acct",   AID_NET_BW_ACCT, },
+    { "qcom_oncrpc",   AID_QCOM_ONCRPC, },
     { "net_bt_stack",  AID_NET_BT_STACK, },
 
     { "misc",          AID_MISC, },
