0e181196f87392edbf6d639a2f442f8949cc647e
Dmitrijs Ledkovs <dmitrijs.ledkovs@canonical.com>
2013-07-08 22:25:54 +0100

    Fix failure to build from source with modern _host_ glibc/gcc.
    One should not define __USE_GNU directly, and instead _GNU_SOURCE
    should be defined as early as possible, before first (indirect)
    inclusion of features.h
    
    Change-Id: I4d109d4fa0c3d266b873d2a0a66a52e1a4faa234
    Signed-off-by: Dmitrijs Ledkovs <dmitrijs.ledkovs@canonical.com>
    
    Conflicts:
    	libcorkscrew/arch-x86/backtrace-x86.c

--- libcorkscrew/arch-x86/backtrace-x86.c
+++ libcorkscrew/arch-x86/backtrace-x86.c
@@ -18,6 +18,12 @@
  * Backtracing functions for x86.
  */
 
+#ifndef __BIONIC__
+// glibc has its own renaming of the Linux kernel's structures.
+#define _GNU_SOURCE // For REG_EBP, REG_ESP, and REG_EIP.
+#include <ucontext.h>
+#endif
+
 #define LOG_TAG "Corkscrew"
 //#define LOG_NDEBUG 0
 
