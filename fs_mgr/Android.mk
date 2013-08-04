d2ad72ee19938b153d66f373b5b10a9f7290d498
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-08-04 13:28:14 +0100

    Remove HAVE_SELINUX guards
    Android 4.3, and by extension CM 10.2, default to having SELinux on
    
    Change-Id: Ie7853727102725a75a7d6fa3b3b35b1ffaacd89a
    
    Conflicts:
    	fs_mgr/Android.mk

--- fs_mgr/Android.mk
+++ fs_mgr/Android.mk
@@ -3,10 +3,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-ifeq ($(HAVE_SELINUX),true)
-LOCAL_CFLAGS += -DHAVE_SELINUX
-endif
-
 LOCAL_SRC_FILES:= fs_mgr.c fs_mgr_verity.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
